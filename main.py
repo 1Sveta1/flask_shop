76b6b2eb5dd378a5fe1e19e027e46c626ca45dc3
1Sveta1 <svetlana.isayko1@gmail.com>
2022-07-27 13:35:20 +0300

    add new file category and

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sl_bd.db'
+db = SQLAlchemy
 
 @app.route("/")
 def hello():
     return "<h2>hello world</h2>"
+
+
+@app.route("/menu/<int:n>")
+def proverka(n):
+        if n == 1:
+            return render_template('menu.html')
+        if n == 2:
+            return render_template('header.html')
+        if n == 3:
+            return render_template('menu.html')
+        if n == 4:
+            return render_template('header.html')
+        if n == 5:
+            return render_template('header.html')
 
 
 @app.route('/menu')
